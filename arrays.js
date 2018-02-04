8bc14a088c0cc450d7974cd342c883a631b04f61
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2018-02-04 22:51:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,9 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 var addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
+
+var destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array 
+}
+
